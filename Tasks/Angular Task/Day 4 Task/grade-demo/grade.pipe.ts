7f38067d0898a81f794a029adf8c716caf40261d
Roshni0613 <89258891+Roshni0613@users.noreply.github.com>
2021-12-23 16:38:02 +0530

    Add files via upload

---
+++ Tasks/Angular Task/Day 4 Task/grade-demo/grade.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'grade'
+})
+export class GradePipe implements PipeTransform {
+
+  transform(grade: number): string {
+    let result="";
+    switch(grade){
+      case 1:
+        result="Outstanding";
+        break;
+      case 2:
+        result="Excellent";
+        break;
+      case 3:
+        result="Good";
+        break;
+      case 4:
+        result="Average";
+        break;
+
+    }
+    return result;
+  }
+
+}
